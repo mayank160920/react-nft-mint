eff0dbb3489a1aecfaf91db7000d16cba671a54e
mayank160920 <ay160920@gmail.com>
2021-09-21 17:11:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-62bybr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-62bybr)
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="header">
+      <img src="" alt="" width="100px" height="50px" />
+      <div className="navItems">
+        <a className="navItem" href="/mint">
+          Mint
+        </a>
+        <a className="navItem" href="/about">
+          About
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
